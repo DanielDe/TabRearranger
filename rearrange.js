634661d7a5d9eca7f587c0cfbc05852da07a57d2
Daniel de Haas <daniel.andrew.dehaas@gmail.com>
2017-10-07 18:40:18 -0700

    Removed Yeoman cruft

---
+++ rearrange.js
@@ -0,0 +1,103 @@
+/* global chrome */
+
+chrome.commands.onCommand.addListener(command => {
+  const indexStep = {
+    'move-tab-left': -1,
+    'move-tab-right': 1,
+    'move-tab-to-next-window': 1
+  }[command];
+
+  const getNumTabsInCurrentWindow = () => {
+    return new Promise((resolve, reject) => {
+      chrome.windows.getCurrent({ populate: true }, window => {
+        resolve(window.tabs.length);
+      });
+    });
+  };
+
+  const getNumPinnedTabsInCurrentWindow = () => {
+    return new Promise((resolve, reject) => {
+      chrome.tabs.query({
+        currentWindow: true,
+        pinned: true
+      }, tabs => {
+        resolve(tabs.length);
+      });
+    });
+  };
+
+  const shiftTabInWindow = tabs => {
+    const currentTab = tabs[0];
+    if (!currentTab) {
+      return;
+    }
+
+    getNumTabsInCurrentWindow().then(numTabs => {
+      getNumPinnedTabsInCurrentWindow().then(numPinnedTabs => {
+        const numUnPinnedTabs = numTabs - numPinnedTabs;
+        let newIndex = currentTab.index + indexStep;
+
+        if (currentTab.pinned) {
+          newIndex = newIndex % numPinnedTabs;
+          if (newIndex === -1) {
+            newIndex = numPinnedTabs - 1;
+          }
+        } else {
+          newIndex = (newIndex - numPinnedTabs) % numUnPinnedTabs + numPinnedTabs;
+          if (newIndex === numPinnedTabs - 1) {
+            newIndex = -1;
+          }
+        }
+        chrome.tabs.move(currentTab.id, { index: newIndex });
+      });
+    });
+  };
+
+  const moveTabToWindow = tabs => {
+    const currentTab = tabs[0];
+
+    chrome.windows.getAll({ windowTypes: ['normal'] }, windows => {
+      const currentWindowIndex = windows.findIndex(window => window.focused);
+      let newIndex = (currentWindowIndex + indexStep) % windows.length;
+
+      if (newIndex === -1) {
+        newIndex = windows.length - 1;
+      }
+      const newWindow = windows[newIndex];
+
+      chrome.tabs.move(currentTab.id, { index: -1, windowId: newWindow.id });
+      chrome.windows.update(newWindow.id, { focused: true });
+      chrome.tabs.update(currentTab.id, { active: true, pinned: currentTab.pinned });
+    });
+  };
+
+  let moveTabToNewWindow = tabs => {
+    const currentTab = tabs[0];
+
+    chrome.windows.getCurrent({ populate: true }, window => {
+      if (window.tabs.length !== 1) {
+        chrome.windows.create({ tabId: currentTab.id });
+      }
+    });
+  };
+
+  const executeCommand = commandCallback => {
+    [true, false].forEach(pinned => {
+      chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+        pinned
+      }, commandCallback);
+    });
+  };
+
+  if (['move-tab-left', 'move-tab-right'].includes(command)) {
+    executeCommand(shiftTabInWindow);
+  } else if (command === 'move-tab-to-next-window') {
+    executeCommand(moveTabToWindow);
+  } else if (command === 'move-tab-to-new-window') {
+    executeCommand(moveTabToNewWindow);
+  } else {
+    alert(`Tab rearranger doesn't recognize the command "${command}"`);
+  }
+});
